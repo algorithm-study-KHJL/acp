bb74f5f01d0420bdbfe8ce1e999a655b2ad73eec
gmelon <hsh1769@naver.com>
2023-04-27 16:01:25 +0900

    chore: project init

---
+++ src/test/java/com/khjl/acp/AcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khjl.acp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
